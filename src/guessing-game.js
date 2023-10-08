8ee9fb91d8b948c697ebe1f63011a83f73036d62
egorerkovich <erkovich.egor@yandex.by>
2023-10-09 02:01:36 +0300

    First attempt

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,37 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.num = 0;
+    }
     setRange(min, max) {
-
+        this.arr = [];
+        this.min = min;
+        this.max = max;
+        for (let i = this.min; i <= this.max; i++) {
+            this.arr.push(i);
+        }
     }
 
     guess() {
-
+        this.num = this.arr[Math.floor(this.arr.length / 2)] 
+        return this.num;
     }
 
     lower() {
-
+        this.arr = [];
+        this.max = this.num;
+        for (let i = this.min; i <= this.max; i++) {
+            this.arr.push(i);
+        }
     }
 
     greater() {
-
+        this.arr = [];
+        this.min = this.num;
+        for (let i = this.min; i <= this.max; i++) {
+            this.arr.push(i);
+        }
     }
 }
 
